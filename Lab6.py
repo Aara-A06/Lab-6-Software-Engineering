33f69647711af9a2ed0a2c4ea1019bce24c95031
Khzhang734 <khzhang734@gmail.com>
2024-10-23 22:08:54 -0400

    Update Lab6.py

--- Lab6.py
+++ Lab6.py
@@ -12,7 +12,7 @@ def encode(password):
         encoded_password += str(encoded_digit)
     return encoded_password
 
-
+# Added by Kevin 
 def decode(encoded_password):
     """
     Decodes the encoded password by shifting each digit down by 3.
@@ -23,33 +23,28 @@ def decode(encoded_password):
         decoded_password += str(decoded_digit)
     return decoded_password
 
-
 def main():
     encoded_password = ""
-
     while True:
         print("\nMenu\n-------------")
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
         option = input("Please enter an option: ")
-
+        
         if option == '1':
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!")
-
         elif option == '2':
             if not encoded_password:
                 print("No password has been encoded yet!")
             else:
                 decoded_password = decode(encoded_password)
                 print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
-
         elif option == '3':
             print("Exiting the program.")
             break
-
         else:
             print("Invalid option, please try again.")
 
